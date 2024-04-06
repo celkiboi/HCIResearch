f2438e4170e6be8c504e91c15f6b10a5cdbf545a
Tomislav Celic <celer.tomislav@gmail.com>
2024-04-06 22:06:22 +0200

    Add auto behaviour for supporting different aspect ratios

--- Assets/Scripts/BackgroundManager.cs
+++ Assets/Scripts/BackgroundManager.cs
@@ -9,9 +9,13 @@ public class BackgroundManager : MonoBehaviour
     [SerializeField]
     GameObject background1;
     [SerializeField]
+    GameObject background2;
+    [SerializeField]
     GameObject ground0;
     [SerializeField]
     GameObject ground1;
+    [SerializeField]
+    GameObject ground2;
 
     [SerializeField]
     float paralaxSpeedMultiplier = 1.4f;
@@ -22,9 +26,9 @@ public class BackgroundManager : MonoBehaviour
 
     void Start()
     {
-        xOffset = background1.transform.position.x - background0.transform.position.x;
+        xOffset = background2.transform.position.x - background0.transform.position.x;
         warpPoint = new
-            (background0.transform.position.x - xOffset,
+            (background0.transform.position.x - xOffset / 2,
             background1.transform.position.y,
             background1.transform.position.z);
     }
@@ -42,8 +46,14 @@ public class BackgroundManager : MonoBehaviour
         if (background1.transform.position.x < warpPoint.x)
         {
             Vector3 position = background1.transform.position;
-            Vector3 otherPosition = background0.transform.position;
+            Vector3 otherPosition = background2.transform.position;
             background1.transform.position = new Vector3(otherPosition.x + xOffset, position.y, position.z);
+        }
+        if (background2.transform.position.x < warpPoint.x)
+        {
+            Vector3 position = background2.transform.position;
+            Vector3 otherPosition = background0.transform.position;
+            background2.transform.position = new Vector3(otherPosition.x + xOffset, position.y, position.z);
         }
 
         if(ground0.transform.position.x < warpPoint.x)
@@ -55,16 +65,24 @@ public class BackgroundManager : MonoBehaviour
         if(ground1.transform.position.x < warpPoint.x)
         {
             Vector3 position = ground1.transform.position;
-            Vector3 otherPosition = ground0.transform.position;
+            Vector3 otherPosition = ground2.transform.position;
             ground1.transform.position = new Vector3(otherPosition.x + xOffset, position.y, position.z);
+        }
+        if(ground2.transform.position.x < warpPoint.x)
+        {
+            Vector3 position = ground2.transform.position;
+            Vector3 otherPosition = ground0.transform.position;
+            ground2.transform.position = new Vector3(otherPosition.x + xOffset, position.y, position.z);
         }
 
         if (hasStarted)
         {
-            background0.transform.Translate(paralaxSpeed * Time.deltaTime * Vector2.left);
-            background1.transform.Translate(paralaxSpeed * Time.deltaTime * Vector2.left);
-            ground0.transform.Translate(GameManager.GameSpeed * Time.deltaTime * Vector2.left);
-            ground1.transform.Translate(GameManager.GameSpeed * Time.deltaTime * Vector2.left);
+            background0.transform.Translate(paralaxSpeed * Vector2.left);
+            background1.transform.Translate(paralaxSpeed * Vector2.left);
+            background2.transform.Translate(paralaxSpeed * Vector2.left);
+            ground0.transform.Translate(GameManager.GameSpeed * Vector2.left);
+            ground1.transform.Translate(GameManager.GameSpeed * Vector2.left);
+            ground2.transform.Translate(GameManager.GameSpeed * Vector2.left);
         }
     }
 
